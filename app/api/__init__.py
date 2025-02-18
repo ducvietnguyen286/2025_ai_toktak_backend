e5c0f3837fe88af3164eaff1e9f931916fea5344
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-02-18 11:24:05 +0700

    Add Base Login

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,9 +3,11 @@ from flask import Blueprint
 from flask_restx import Api
 
 from app.api.maker import ns as maker_ns
+from app.api.auth import ns as auth_ns
 
 bp = Blueprint("api", __name__, url_prefix="/api/v1")
 
 api = Api(bp, version="1.0", title="Flask API", description="Flask API", doc="/docs/")
 
 api.add_namespace(ns=maker_ns)
+api.add_namespace(ns=auth_ns)
---
+++ app/services/auth.py
@@ -0,0 +1,50 @@
+from app.errors.exceptions import BadRequest
+from app.models.user import User
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    get_jwt_identity,
+)
+
+
+class AuthService:
+
+    @staticmethod
+    def register(email, password, username=""):
+        user = User.query.filter_by(email=email).first()
+        if user:
+            raise BadRequest(message="Email already exists")
+        user = User(email=email, username=username)
+        user.set_password(password)
+        user.save()
+        return user
+
+    @staticmethod
+    def login(email, password):
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            user = User.query.filter_by(username=email).first()
+        if not user or not user.check_password(password):
+            raise BadRequest(message="Email or password is incorrect")
+        return user
+
+    @staticmethod
+    def generate_token(user):
+        access_token = create_access_token(identity=user.id)
+        refresh_token = create_refresh_token(identity=user.id)
+        return {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+        }
+
+    @staticmethod
+    def refresh_token():
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user)
+        return {"access_token": access_token}
+
+    @staticmethod
+    def get_current_identity():
+        user_id = get_jwt_identity()
+        user = User.query.get(user_id)
+        return user
