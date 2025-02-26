f86535fb81511010a16d68f59d53252eb8a938c0
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-02-26 12:53:22 +0700

    Add Param Person

--- app/models/social_account.py
+++ app/models/social_account.py
@@ -10,6 +10,3 @@ class SocialAccount(db.Model, BaseModel):
     provider = db.Column(db.String(50), index=True, nullable=False)
     provider_user_id = db.Column(db.String(100), nullable=False)
     access_token = db.Column(db.Text, nullable=False)
-    refresh_token = db.Column(db.Text, nullable=True)
-    access_token_expired = db.Column(db.String(100), nullable=True)
-    refresh_token_expired = db.Column(db.String(100), nullable=True)
