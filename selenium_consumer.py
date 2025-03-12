814b447aafeb739630dcd5ada8eb7dd7420c0f3e
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-03-12 15:07:46 +0700

    Fix Bug selenium

--- selenium_consumer.py
+++ selenium_consumer.py
@@ -283,9 +283,9 @@ def process_task_on_tab(browser, task):
 
             logger.info("[Wait Done]")
 
-            # file_html = open("demo2.html", "w", encoding="utf-8")
-            # file_html.write(browser.page_source)
-            # file_html.close()
+            file_html = open("demo2.html", "w", encoding="utf-8")
+            file_html.write(browser.page_source)
+            file_html.close()
 
             browser_cookie = browser.get_cookies()
             formatted_cookies = []
