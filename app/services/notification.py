49eb7d681d731de13d50b231d686571494ef022a
Nguyễn Chí Bảo <chibao2704@gmail.com>
2025-03-19 17:29:53 +0700

    no message

--- app/services/notification.py
+++ app/services/notification.py
@@ -15,6 +15,13 @@ class NotificationServices:
     def find(id):
         return Notification.query.get(id)
     
+    
+    @staticmethod
+    def update_notification(id, *args, **kwargs):
+        notification = Notification.query.get(id)
+        notification.update(**kwargs)
+        return notification
+    
     @staticmethod
     def find_notification_sns(post_id , notification_type):
         notification = Notification.query.filter(
