3867a287f75f3758edd4288cc9be54f1a5c23d94
Nguyễn Chí Bảo <chibao2704@gmail.com>
2025-03-31 17:51:19 +0700

    BaoNC change code

--- app/lib/string.py
+++ app/lib/string.py
@@ -36,7 +36,7 @@ def get_level_images(level):
     """
     Trả về danh sách ảnh theo cấp độ với một ảnh ngẫu nhiên được đánh dấu active.
     """
-    base_url = "/"
+    base_url = "/img/level/"
     images = []
 
     if level == 0:
