4383460c983292dbde205a4f36e29471cd8ffdaa
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-04-04 18:06:46 +0700

    Fix Prompt

--- app/api/maker.py
+++ app/api/maker.py
@@ -1029,10 +1029,6 @@ class APIGetStatusUploadWithBatch(Resource):
                             sns_status = sns_post_detail["status"]
                             notification_type = sns_post_detail["title"]
 
-                            logger.info(f"sns_post_detail: {sns_post_detail}")
-                            logger.info(f"sns_post_id: {sns_post_id}")
-                            logger.info(f"sns_status: {sns_status}")
-                            logger.info(f"notification_type: {notification_type}")
                             notification = NotificationServices.find_notification_sns(
                                 sns_post_id, notification_type
                             )
