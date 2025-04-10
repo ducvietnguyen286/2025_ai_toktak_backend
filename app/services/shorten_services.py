92902d505c8d48975643cce954cce6e9512fa480
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-04-10 13:21:48 +0700

    Add

--- app/services/shorten_services.py
+++ app/services/shorten_services.py
@@ -1,5 +1,6 @@
+import hashlib
 from app.models.shorten import ShortenURL
-from app.lib.string import generate_shortcode
+from app.lib.string import generate_shortcode, should_replace_shortlink
 
 
 class ShortenServices:
@@ -27,3 +28,22 @@ class ShortenServices:
             short_code = generate_shortcode()
             exist = ShortenURL.query.filter_by(short_code=short_code).first()
         return short_code
+
+    @staticmethod
+    def shorted_link(url):
+        if should_replace_shortlink(url):
+            origin_hash = hashlib.sha256(url.encode()).hexdigest()
+            existing_entry = ShortenServices.get_short_by_original_url(origin_hash)
+            domain_share_url = "https://s.toktak.ai/"
+            if not existing_entry:
+                short_code = ShortenServices.make_short_url(url)
+
+                existing_entry = ShortenServices.create_shorten(
+                    original_url=url,
+                    original_url_hash=origin_hash,
+                    short_code=short_code,
+                )
+
+            shorten_link = f"{domain_share_url}{existing_entry.short_code}"
+            return shorten_link, True
+        return url, False
