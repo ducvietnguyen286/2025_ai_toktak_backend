3de2dabf6b603f3d897c746ec948126526cc06dd
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-04-22 09:18:28 +0100

    Fix Cutout

--- ocr.py
+++ ocr.py
@@ -5,35 +5,42 @@ import logging
 from fastapi import FastAPI, Request
 from paddleocr import PaddleOCR
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s: %(message)s", datefmt="%d-%m-%Y %H:%M:%S"
-)
-
 os.makedirs("logs", exist_ok=True)
-now_date = datetime.datetime.now()
-filename = now_date.strftime("%d-%m-%Y")
 
-handler = handlers.TimedRotatingFileHandler(
-    "logs/paddleocr-{0}.log".format(filename),
-    when="midnight",
-    interval=1,
-    backupCount=14,
-    encoding="utf-8",
-)
-handler.setLevel(logging.INFO)
-handler.setFormatter(formatter)
 
-errorLogHandler = handlers.RotatingFileHandler(
-    "logs/paddleocr-error-{0}.log".format(filename), backupCount=14, encoding="utf-8"
-)
-errorLogHandler.setLevel(logging.ERROR)
-errorLogHandler.setFormatter(formatter)
+def create_logger():
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s: %(message)s", datefmt="%d-%m-%Y %H:%M:%S"
+    )
 
-logger.addHandler(handler)
-logger.addHandler(errorLogHandler)
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    now_date = datetime.datetime.now()
+    filename = now_date.strftime("%d-%m-%Y")
+
+    handler = handlers.TimedRotatingFileHandler(
+        "logs/paddleocr-{0}.log".format(filename),
+        when="midnight",
+        interval=1,
+        backupCount=14,
+        encoding="utf-8",
+    )
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+
+    errorLogHandler = handlers.RotatingFileHandler(
+        "logs/paddleocr-error-{0}.log".format(filename),
+        backupCount=14,
+        encoding="utf-8",
+    )
+    errorLogHandler.setLevel(logging.ERROR)
+    errorLogHandler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    logger.addHandler(errorLogHandler)
+
+    return logger
+
 
 MODEL_DIR = os.path.join(os.getcwd(), "app/ais/models/paddleocr_models")
 det_model_dir = os.path.join(MODEL_DIR, "det/Multilingual_PP-OCRv3_det_infer")
@@ -41,22 +48,28 @@ rec_model_dir = os.path.join(MODEL_DIR, "rec/korean_PP-OCRv3_rec_infer")
 
 app = FastAPI()
 
-try:
-    logger.info("Initializing PaddleOCR...")
-    ocr = PaddleOCR(
-        use_gpu=True,
-        lang="korean",
-        det_model_dir=det_model_dir,
-        rec_model_dir=rec_model_dir,
-    )
-    logger.info("PaddleOCR initialized successfully.")
-except Exception as e:
-    logger.error(f"Error initializing PaddleOCR: {e}")
-    raise
+
+def initialize_ocr_model():
+    logger = create_logger()
+    try:
+        logger.info("Initializing PaddleOCR...")
+        ocr = PaddleOCR(
+            use_gpu=True,
+            lang="korean",
+            det_model_dir=det_model_dir,
+            rec_model_dir=rec_model_dir,
+        )
+        logger.info("PaddleOCR initialized successfully.")
+        return ocr
+    except Exception as e:
+        logger.error(f"Error initializing PaddleOCR: {e}")
+        raise
 
 
 @app.post("/check_text")
 async def check_text(request: Request):
+    logger = create_logger()
+    ocr = initialize_ocr_model()
     try:
         logger.info("Step 1: Received request")
         data = await request.json()
