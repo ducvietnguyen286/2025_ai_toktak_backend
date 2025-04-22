25b08c4f67ce328cb8d110c7d0e481f06ca1f44f
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-04-22 10:36:39 +0100

    Fix Cutout done

--- ocr.py
+++ ocr.py
@@ -94,13 +94,9 @@ async def check_text(request: Request):
         for line_group in result:
             for line in line_group:
                 detection = line[0]
-                x_coords = [pt[0] for pt in detection]
-                y_coords = [pt[1] for pt in detection]
-
-                box_width = max(x_coords) - min(x_coords)
-                box_height = max(y_coords) - min(y_coords)
-                box_area = box_width * box_height
-                sum_text_area += box_area
+                pts = np.array(detection, dtype=np.int32)
+                area = cv2.contourArea(pts)
+                total_text_area += area
 
                 texts.append(line[1][0])
 
