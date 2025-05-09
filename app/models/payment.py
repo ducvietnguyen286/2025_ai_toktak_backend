0909f1ff6f4e270b742f55998a10e06018a2440c
Nguyễn Chí Bảo <chibao2704@gmail.com>
2025-05-09 17:15:09 +0700

    BaoNC change code

--- app/models/payment.py
+++ app/models/payment.py
@@ -1,5 +1,6 @@
 from app.extensions import db
 from app.models.base import BaseModel
+from datetime import datetime
 
 
 class Payment(db.Model, BaseModel):
@@ -7,9 +8,14 @@ class Payment(db.Model, BaseModel):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"), nullable=False)
+    order_id = db.Column(db.Integer,  nullable=False)
+    package_name = db.Column(db.String(50), nullable=False)
     payment_method = db.Column(db.String(50), nullable=False)
     payment_status = db.Column(db.String(50), nullable=False)
     payment_date = db.Column(db.DateTime, nullable=False)
+    price = db.Column(db.Float, nullable=False)
     amount = db.Column(db.Float, nullable=False)
     status = db.Column(db.Integer, default=1)
+
+    start_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    end_date = db.Column(db.DateTime, nullable=False)
