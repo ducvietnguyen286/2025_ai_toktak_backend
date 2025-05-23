444c4272bc7a405b3bc8cae82a2a9921e6c79cbf
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-05-23 11:20:08 +0700

    Change Using Mongoengine

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,15 @@
 # coding: utf8
+
 from logging import DEBUG
 import os
 
 from werkzeug.exceptions import default_exceptions
 
-from app.lib.logger import log_mongo_database
-
 from .errors.handler import api_error_handler
 
 from flask import Flask, jsonify
 from flask_cors import CORS
-from .extensions import redis_client, db, bcrypt, jwt, db_mongo, make_celery
+from .extensions import redis_client, db, bcrypt, jwt, init_mongoengine, make_celery
 from pymongo import monitoring
 
 from flask_jwt_extended.exceptions import NoAuthorizationError
@@ -68,7 +67,7 @@ def __init_app(app):
     redis_client.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
-    db_mongo.init_app(app)
+    init_mongoengine(app)
     # init_sam_model(app)
 
     celery = make_celery(app)
