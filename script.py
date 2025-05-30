9e5babb132f79b01e90a61cfdc6849f37aec4cc4
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-05-30 10:30:00 +0700

    Change App Script

--- script.py
+++ script.py
@@ -51,195 +51,188 @@ def create_app():
 
 def main():
     app = create_app()
-    import_batch_data(app)
-    # import_image_template()
-    # import_month_text()
-    # import_youtube_client()
+    with app.app_context():
+        import_image_template()
+        import_month_text()
+        import_youtube_client()
+        import_batch_data()
 
 
 def import_image_template():
-    app = create_app()
-    with app.app_context():
-        print("Start Script...")
-        json_path = os.path.join(
-            os.path.dirname(__file__), "data_scripts", "toktak.image_templates.json"
-        )
-        with open(json_path, "r", encoding="utf-8") as f:
-            data = json.load(f)
+    print("Start Script...")
+    json_path = os.path.join(
+        os.path.dirname(__file__), "data_scripts", "toktak.image_templates.json"
+    )
+    with open(json_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
 
-        try:
-            db.session.execute(text("TRUNCATE TABLE image_templates;"))
-            for item in data:
-                it = ImageTemplate(
-                    created_at=datetime.fromisoformat(
-                        item["created_at"]["$date"].replace("Z", "+00:00")
-                    ),
-                    updated_at=datetime.fromisoformat(
-                        item["updated_at"]["$date"].replace("Z", "+00:00")
-                    ),
-                    template_name=item.get("template_name"),
-                    template_code=item.get("template_code"),
-                    template_image=item.get("template_image"),
-                    font=item.get("font"),
-                    font_name=item.get("font_name"),
-                    font_path=item.get("font_path"),
-                    font_size=item.get("font_size"),
-                    main_text_color=item.get("main_text_color"),
-                    text_color=item.get("text_color"),
-                    stroke_color=item.get("stroke_color"),
-                    stroke_width=item.get("stroke_width"),
-                    text_shadow=item.get("text_shadow"),
-                    text_align=item.get("text_align"),
-                    text_position=item.get("text_position"),
-                    text_position_x=item.get("text_position_x"),
-                    text_position_y=item.get("text_position_y"),
-                    background=item.get("background"),
-                    background_color=item.get("background_color"),
-                    background_image=item.get("background_image"),
-                    padding=item.get("padding"),
-                    margin=item.get("margin"),
-                    type=item.get("type"),
-                    created_by=item.get("created_by"),
-                    sort=item.get("sort"),
-                    status=item.get("status"),
-                )
-                db.session.add(it)
+    try:
+        db.session.execute(text("TRUNCATE TABLE image_templates;"))
+        for item in data:
+            it = ImageTemplate(
+                created_at=datetime.fromisoformat(
+                    item["created_at"]["$date"].replace("Z", "+00:00")
+                ),
+                updated_at=datetime.fromisoformat(
+                    item["updated_at"]["$date"].replace("Z", "+00:00")
+                ),
+                template_name=item.get("template_name"),
+                template_code=item.get("template_code"),
+                template_image=item.get("template_image"),
+                font=item.get("font"),
+                font_name=item.get("font_name"),
+                font_path=item.get("font_path"),
+                font_size=item.get("font_size"),
+                main_text_color=item.get("main_text_color"),
+                text_color=item.get("text_color"),
+                stroke_color=item.get("stroke_color"),
+                stroke_width=item.get("stroke_width"),
+                text_shadow=item.get("text_shadow"),
+                text_align=item.get("text_align"),
+                text_position=item.get("text_position"),
+                text_position_x=item.get("text_position_x"),
+                text_position_y=item.get("text_position_y"),
+                background=item.get("background"),
+                background_color=item.get("background_color"),
+                background_image=item.get("background_image"),
+                padding=item.get("padding"),
+                margin=item.get("margin"),
+                type=item.get("type"),
+                created_by=item.get("created_by"),
+                sort=item.get("sort"),
+                status=item.get("status"),
+            )
+            db.session.add(it)
 
-            db.session.commit()
-            db.session.close()
-        except Exception as e:
-            traceback.print_exc()
-            print(f"Error reading JSON file: {e}")
-            return
-        print("End Script...")
+        db.session.commit()
+        db.session.close()
+    except Exception as e:
+        traceback.print_exc()
+        print(f"Error reading JSON file: {e}")
+        return
+    print("End Script...")
 
 
 def import_month_text():
-    app = create_app()
-    with app.app_context():
-        print("Start Script...")
-        json_path = os.path.join(
-            os.path.dirname(__file__), "data_scripts", "toktak.month_texts.json"
-        )
-        with open(json_path, "r", encoding="utf-8") as f:
-            data = json.load(f)
+    print("Start Script...")
+    json_path = os.path.join(
+        os.path.dirname(__file__), "data_scripts", "toktak.month_texts.json"
+    )
+    with open(json_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
 
-        try:
-            db.session.execute(text("TRUNCATE TABLE month_texts;"))
-            for item in data:
-                created_at_str = item.get("created_at", {}).get("$date")
-                updated_at_str = item.get("updated_at", {}).get("$date")
+    try:
+        db.session.execute(text("TRUNCATE TABLE month_texts;"))
+        for item in data:
+            created_at_str = item.get("created_at", {}).get("$date")
+            updated_at_str = item.get("updated_at", {}).get("$date")
 
-                if type(item.get("keyword")) is not str:
-                    print(f"Invalid keyword format for item: {item}")
-                    continue
+            if type(item.get("keyword")) is not str:
+                print(f"Invalid keyword format for item: {item}")
+                continue
 
-                if type(item.get("hashtag")) is not str:
-                    print(f"Invalid hashtag format for item: {item}")
-                    continue
+            if type(item.get("hashtag")) is not str:
+                print(f"Invalid hashtag format for item: {item}")
+                continue
 
-                if type(item.get("month")) is not str:
-                    print(f"Invalid month format for item: {item}")
-                    continue
+            if type(item.get("month")) is not str:
+                print(f"Invalid month format for item: {item}")
+                continue
 
-                if type(item.get("status")) is not str:
-                    print(f"Invalid status format for item: {item}")
-                    continue
+            if type(item.get("status")) is not str:
+                print(f"Invalid status format for item: {item}")
+                continue
 
-                it = MonthText(
-                    created_at=(
-                        datetime.fromisoformat(created_at_str.replace("Z", "+00:00"))
-                        if created_at_str
-                        else None
-                    ),
-                    updated_at=(
-                        datetime.fromisoformat(updated_at_str.replace("Z", "+00:00"))
-                        if updated_at_str
-                        else None
-                    ),
-                    keyword=item.get("keyword"),
-                    hashtag=item.get("hashtag"),
-                    month=item.get("month"),
-                    status=item.get("status"),
-                )
-                db.session.add(it)
+            it = MonthText(
+                created_at=(
+                    datetime.fromisoformat(created_at_str.replace("Z", "+00:00"))
+                    if created_at_str
+                    else None
+                ),
+                updated_at=(
+                    datetime.fromisoformat(updated_at_str.replace("Z", "+00:00"))
+                    if updated_at_str
+                    else None
+                ),
+                keyword=item.get("keyword"),
+                hashtag=item.get("hashtag"),
+                month=item.get("month"),
+                status=item.get("status"),
+            )
+            db.session.add(it)
 
-            db.session.commit()
-            db.session.close()
-        except Exception as e:
-            traceback.print_exc()
-            print(f"Error reading JSON file: {e}")
-            return
-        print("End Script...")
+        db.session.commit()
+        db.session.close()
+    except Exception as e:
+        traceback.print_exc()
+        print(f"Error reading JSON file: {e}")
+        return
+    print("End Script...")
 
 
 def import_youtube_client():
-    app = create_app()
-    with app.app_context():
-        print("Start Script...")
-        json_path = os.path.join(
-            os.path.dirname(__file__), "data_scripts", "toktak.youtube_clients.json"
-        )
-        with open(json_path, "r", encoding="utf-8") as f:
-            data = json.load(f)
+    print("Start Script...")
+    json_path = os.path.join(
+        os.path.dirname(__file__), "data_scripts", "toktak.youtube_clients.json"
+    )
+    with open(json_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
 
-        try:
-            db.session.execute(text("TRUNCATE TABLE youtube_clients;"))
-            for item in data:
-                created_at_str = item.get("created_at", {}).get("$date")
-                updated_at_str = item.get("updated_at", {}).get("$date")
-                it = YoutubeClient(
-                    created_at=(
-                        datetime.fromisoformat(created_at_str.replace("Z", "+00:00"))
-                        if created_at_str
-                        else None
-                    ),
-                    updated_at=(
-                        datetime.fromisoformat(updated_at_str.replace("Z", "+00:00"))
-                        if updated_at_str
-                        else None
-                    ),
-                    user_ids=json.dumps(item.get("user_ids", [])),
-                    member_count=item.get("member_count"),
-                    project_name=item.get("project_name"),
-                    client_id=item.get("client_id"),
-                    client_secret=item.get("client_secret"),
-                )
-                db.session.add(it)
+    try:
+        db.session.execute(text("TRUNCATE TABLE youtube_clients;"))
+        for item in data:
+            created_at_str = item.get("created_at", {}).get("$date")
+            updated_at_str = item.get("updated_at", {}).get("$date")
+            it = YoutubeClient(
+                created_at=(
+                    datetime.fromisoformat(created_at_str.replace("Z", "+00:00"))
+                    if created_at_str
+                    else None
+                ),
+                updated_at=(
+                    datetime.fromisoformat(updated_at_str.replace("Z", "+00:00"))
+                    if updated_at_str
+                    else None
+                ),
+                user_ids=json.dumps(item.get("user_ids", [])),
+                member_count=item.get("member_count"),
+                project_name=item.get("project_name"),
+                client_id=item.get("client_id"),
+                client_secret=item.get("client_secret"),
+            )
+            db.session.add(it)
 
-            db.session.commit()
-            db.session.close()
-        except Exception as e:
-            traceback.print_exc()
-            print(f"Error reading JSON file: {e}")
-            return
-        print("End Script...")
+        db.session.commit()
+        db.session.close()
+    except Exception as e:
+        traceback.print_exc()
+        print(f"Error reading JSON file: {e}")
+        return
+    print("End Script...")
 
 
 def logout_x():
-    app = create_app()
-    with app.app_context():
-        print("Start Script...")
-        users = UserService.all_users()
-        links = LinkService.get_all_links()
-        x_link = None
-        for link in links:
-            if link.get("type") == "X":
-                x_link = link
-                break
-        for user in users:
-            user_id = user.get("id")
-            x_link_id = x_link.get("id")
-            user_link = UserService.find_user_link(user_id=user_id, link_id=x_link_id)
-            if user_link:
-                UserService.delete_user_link(user_link_id=user_link.id)
-                NotificationServices.create_notification(
-                    user_id=user.get("id"),
-                    title=f"🔒 X 계정의 토큰이 만료됐어요!",
-                    description="🔗 계속 사용하시려면 X 계정을 다시 연결해 주세요. 😊",
-                    description_korea="🔗 계속 사용하시려면 X 계정을 다시 연결해 주세요. 😊",
-                )
-        print("End Script...")
+    print("Start Script...")
+    users = UserService.all_users()
+    links = LinkService.get_all_links()
+    x_link = None
+    for link in links:
+        if link.get("type") == "X":
+            x_link = link
+            break
+    for user in users:
+        user_id = user.get("id")
+        x_link_id = x_link.get("id")
+        user_link = UserService.find_user_link(user_id=user_id, link_id=x_link_id)
+        if user_link:
+            UserService.delete_user_link(user_link_id=user_link.id)
+            NotificationServices.create_notification(
+                user_id=user.get("id"),
+                title=f"🔒 X 계정의 토큰이 만료됐어요!",
+                description="🔗 계속 사용하시려면 X 계정을 다시 연결해 주세요. 😊",
+                description_korea="🔗 계속 사용하시려면 X 계정을 다시 연결해 주세요. 😊",
+            )
+    print("End Script...")
 
 
 if __name__ == "__main__":
