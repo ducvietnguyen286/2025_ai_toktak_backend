ca666622f0b35038163ce297eeadac5b7449f918
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-06-02 21:00:24 +0700

    Disable Create Content AI cache

--- app/ais/chatgpt.py
+++ app/ais/chatgpt.py
@@ -86,9 +86,9 @@ def get_from_database(type, data):
 
 
 def call_chatgpt_create_caption(images=[], data={}, post_id=""):
-    response_database = get_from_database("video", data)
-    if response_database:
-        return response_database
+    # response_database = get_from_database("video", data)
+    # if response_database:
+    #     return response_database
     prompt = """[역할]
 당신은 SNS 숏폼 콘텐츠 전문가입니다.
 사용자의 Pain Point(불편함, 고민, 불만 등)를 중심으로 문제를 제시하고,
@@ -234,18 +234,18 @@ hashtag:
     }
 
     response = call_chatgpt(content, response_schema, post_id)
-    if response:
-        if "error" in response:
-            return response
-        save_to_database("video", data, response)
+    # if response:
+    #     if "error" in response:
+    #         return response
+    #     save_to_database("video", data, response)
 
     return response
 
 
 def call_chatgpt_create_blog(images=[], data={}, post_id=0):
-    response_database = get_from_database("blog", data)
-    if response_database:
-        return response_database
+    # response_database = get_from_database("blog", data)
+    # if response_database:
+    #     return response_database
 
     prompt = """업로드된 이미지들을 참고하여, 제품의 다음 세부 정보를 반영한 블로그 게시글을 작성해 주세요.
 
@@ -401,18 +401,18 @@ Note:
     }
 
     response = call_chatgpt(content, response_schema, post_id)
-    if response:
-        if "error" in response:
-            return response
-        save_to_database("blog", data, response)
+    # if response:
+    #     if "error" in response:
+    #         return response
+    #     save_to_database("blog", data, response)
 
     return response
 
 
 def call_chatgpt_create_social(images=[], data={}, post_id=0):
-    response_database = get_from_database("image", data)
-    if response_database:
-        return response_database
+    # response_database = get_from_database("image", data)
+    # if response_database:
+    #     return response_database
 
     prompt = """[역할]  
 당신은 SNS 바이럴 콘텐츠 제작 전문가입니다.  
@@ -517,10 +517,10 @@ hashtag
     }
 
     response = call_chatgpt(content, response_schema, post_id)
-    if response:
-        if "error" in response:
-            return response
-        save_to_database("image", data, response)
+    # if response:
+    #     if "error" in response:
+    #         return response
+    #     save_to_database("image", data, response)
 
     return response
 
