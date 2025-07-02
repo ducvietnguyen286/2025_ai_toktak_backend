7e8c72280bca349653fc871bbc7de9215c71cffd
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-07-02 11:01:42 +0100

    Add Coupang

--- app/scraper/__init__.py
+++ app/scraper/__init__.py
@@ -41,44 +41,44 @@ def get_page_scraper(params):
 class Scraper:
     def scraper(self, params):
         response = get_page_scraper(params)
-        if not response:
-            parsed_url = urlparse(params["url"])
-            netloc = parsed_url.netloc
-            logger.info(netloc)
-            params["netloc"] = netloc
-            urls = [
-                "https://scraper.vodaplay.vn/api/v1/maker/create-scraper",
-                "https://apitoktak.voda-play.com/api/v1/maker/create-scraper",
-                "https://scraper.play-tube.net/api/v1/maker/create-scraper",
-                "https://scraper.canvasee.com/api/v1/maker/create-scraper",
-                "https://scraper.bodaplay.ai/api/v1/maker/create-scraper",
-            ]
-            random.shuffle(urls)
-            for url in urls:
-                new_response = Scraper().call_api_and_get_data(url, params)
-                if new_response:
-                    response = new_response
+        # if not response:
+        #     parsed_url = urlparse(params["url"])
+        #     netloc = parsed_url.netloc
+        #     logger.info(netloc)
+        #     params["netloc"] = netloc
+        #     urls = [
+        #         "https://scraper.vodaplay.vn/api/v1/maker/create-scraper",
+        #         "https://apitoktak.voda-play.com/api/v1/maker/create-scraper",
+        #         "https://scraper.play-tube.net/api/v1/maker/create-scraper",
+        #         "https://scraper.canvasee.com/api/v1/maker/create-scraper",
+        #         "https://scraper.bodaplay.ai/api/v1/maker/create-scraper",
+        #     ]
+        #     random.shuffle(urls)
+        #     for url in urls:
+        #         new_response = Scraper().call_api_and_get_data(url, params)
+        #         if new_response:
+        #             response = new_response
 
-                    real_url = get_real_url(params["url"])
+        #             real_url = get_real_url(params["url"])
 
-                    crawl_url_hash = hashlib.sha1(real_url.encode()).hexdigest()
-                    # Check exists với crawl_url_hash
-                    exists = CrawlDataService.find_crawl_data(crawl_url_hash)
-                    if (
-                        not exists
-                        and response
-                        and "images" in response
-                        and len(response["images"]) > 0
-                    ):
-                        site = get_site_by_url(real_url)
-                        CrawlDataService.create_crawl_data(
-                            site=site,
-                            input_url=params["url"],
-                            crawl_url=real_url,
-                            crawl_url_hash=crawl_url_hash,
-                            response=json.dumps(new_response),
-                        )
-                    break
+        #             crawl_url_hash = hashlib.sha1(real_url.encode()).hexdigest()
+        #             # Check exists với crawl_url_hash
+        #             exists = CrawlDataService.find_crawl_data(crawl_url_hash)
+        #             if (
+        #                 not exists
+        #                 and response
+        #                 and "images" in response
+        #                 and len(response["images"]) > 0
+        #             ):
+        #                 site = get_site_by_url(real_url)
+        #                 CrawlDataService.create_crawl_data(
+        #                     site=site,
+        #                     input_url=params["url"],
+        #                     crawl_url=real_url,
+        #                     crawl_url_hash=crawl_url_hash,
+        #                     response=json.dumps(new_response),
+        #                 )
+        #             break
 
         return response
 
