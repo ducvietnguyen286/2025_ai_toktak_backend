90c23ddd5b3cb1614bc6e1ca3f8d59475a2711b9
Nguyễn Hồng Linh <jd.linhnh95@gmail.com>
2025-07-31 14:28:39 +0700

    Check Log

--- app/services/user_link.py
+++ app/services/user_link.py
@@ -16,6 +16,8 @@ class UserLinkService:
         user_link = UserService.find_user_link(link_id=link.id, user_id=user_id)
         user_link_id = user_link.id if user_link else 0
 
+        print(f"user_link_id: {user_link_id}")
+
         if link.type == "X":
             UserService.update_user_link(
                 id=user_link_id,
